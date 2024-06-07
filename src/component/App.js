dc938d9a95aa548ce9193b352332a97fe13d5daa
Yihui01251 <ciuhyi@gmail.com>
2024-06-07 17:01:39 +0800

    改cra環境

--- src/component/App.js
+++ src/component/App.js
@@ -2,7 +2,6 @@ import { useEffect,useState } from "react";
 import axios from "axios";
 import $ from "jquery";
 //外部檔案
-import logo from '../images/logo.svg';
 import '../css/App.css';
 import Input from './Input';
 import '../styleSheet/css/style.css'
@@ -22,22 +21,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div id="jQtext"></div>
-        <button type="button" class="btn btn-primary" onClick={()=>{$(function(){$('#jQtext').text('jQuery').css({color:'red'});  })}}>Primary</button>
-        <p>
-          i i i 
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Input id="12test" value={account} onChangHandler={(e)=>{setAccount(e.target.value)}}/>
+       
+     
       </header>
     </div>
   );
